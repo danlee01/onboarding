351018a2c148f8a831fbd052c10d72d47e42ac35
Dan Lee <danlee01@ucla.edu>
2021-12-03 17:07:57 -0800

    almost milestone 3, app has input and display children

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+import PostDisplay from './components/PostDisplay';
+import PostInput from './components/PostInput';
+
+const App = function () {
+  const [submitted, setSubmitted] = useState(false);
+
+  const refreshDisplay = function () {
+    setSubmitted(true);
+    setSubmitted(false);
+  };
+
+  return (
+    <div>
+      <PostInput submitted={submitted} />
+      <PostDisplay refreshDisplay={refreshDisplay} />
+    </div>
+  );
+};
+
+/*
+const App = function () {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
@@ -20,6 +44,6 @@ function App() {
       </header>
     </div>
   );
-}
-
+};
+*/
 export default App;
